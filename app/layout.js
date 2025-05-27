39c93745d32327e1840c358f500849e63b46b6a1
InolofatsengMotloba <motlobainolo123@gmail.com>
2025-05-27 14:55:05 +0200

    Import Header component and display it on layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main>{children}</main>
         <Footer />
       </body>
